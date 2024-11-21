2409cb14ca358d9df74f3634e266396abce07d4a
Mateusz <mateusz.jablonski896@gmail.com>
2024-11-21 18:15:30 +0100

    changed slick breakpoints

--- js/slick.js
+++ js/slick.js
@@ -1,18 +1,26 @@
 $(document).ready(function () {
-    $('.reviews__slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        arrows: false,
-        mobileFirst: true,
-        responsive: [{
-            breakpoint: 700,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        }],
-
-    });
+	$('.reviews__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		arrows: false,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	})
 })
